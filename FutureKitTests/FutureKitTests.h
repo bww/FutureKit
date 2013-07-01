16bd42e4b60dd9d2c672b403ef394c4407be09e5
Brian William Wolter <brian@woltergroup.net>
2013-07-01 17:20:36 -0400

    Initial Commit

---
+++ FutureKitTests/FutureKitTests.h
@@ -0,0 +1,13 @@
+//
+//  FutureKitTests.h
+//  FutureKitTests
+//
+//  Created by Brian William Wolter on 7/1/13.
+//  Copyright (c) 2013 Brian William Wolter. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface FutureKitTests : SenTestCase
+
+@end
---
+++ FutureKitTests/FutureKitTests.m
@@ -0,0 +1,32 @@
+//
+//  FutureKitTests.m
+//  FutureKitTests
+//
+//  Created by Brian William Wolter on 7/1/13.
+//  Copyright (c) 2013 Brian William Wolter. All rights reserved.
+//
+
+#import "FutureKitTests.h"
+
+@implementation FutureKitTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in FutureKitTests");
+}
+
+@end
